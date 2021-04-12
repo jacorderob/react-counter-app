59cb8e4812a275385df08d069f7634fca56fcf77
Andrés Cordero <jrcorder@uc.cl>
2021-04-11 21:15:46 -0400

    Print variables in html

--- src/FirstApp.js
+++ src/FirstApp.js
@@ -2,7 +2,21 @@ import React from 'react';
 
 const FirstApp = () => {
 
-  return <h1>Hello World!</h1>;
+  const greeting = 'Hi! Waraaap'
+
+  const obj = {
+    name: 'Andrés',
+    age: 29
+  }
+
+  return (
+    <>
+      <h1>{greeting}</h1>
+      <h2>{JSON.stringify(obj)}</h2>
+      <pre>{JSON.stringify(obj, null, 2)}</pre>
+      <p>Mi primera aplicacion</p>
+    </>
+  );
 }
 
 export default FirstApp;
